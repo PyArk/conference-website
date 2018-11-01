f7dff9d465a634403623a8dd1ae199a7df7902ec
adamclmns <adam@adamclmns.com>
2018-11-01 16:54:38 -0500

    Following this guide - https://devcenter.heroku.com/articles/django-assets

--- pyconark/settings.py
+++ pyconark/settings.py
@@ -56,7 +56,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'pyconark.urls'
 
